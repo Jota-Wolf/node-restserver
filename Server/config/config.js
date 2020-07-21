e22329c62355547ac7ff7a44a2bd8c1f41bf5436
Jota-Wolf <lobos.joaquin@hotmail.com>
2020-07-20 20:01:02 -0400

    :sparkles: Variable de entorno MONGO

--- Server/config/config.js
+++ Server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jota:y0mq4tA4iELZJ4ma@cluster0.rkjzl.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
